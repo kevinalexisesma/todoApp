a18ad9d99416ed6e498d0927bbd39a9bb6993acc
Leonardo <leonardotrve@ufps.edu.co>
2024-06-06 00:56:34 -0500

    feat:add_filters_passwork

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,5 +8,29 @@ class ProfileCrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'password', 'emil', 'bio']
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data.get('password'))
+        validated_data["password"] = serializers.CharField(write_only=True, required=True, validators=[
+                RegexValidator(
+                    regex=r'^(?=.*[A-Z])',  # Al menos una letra mayúscula
+                    message=('The password must contain at least one uppercase letter.')
+                ),
+                RegexValidator(
+                    regex=r'^(?=.*[a-z])',  # Al menos una letra minúscula
+                    message=('The password must contain at least one lowercase letter.')
+                ),
+                RegexValidator(
+                    regex=r'^(?=.*\d)',  # Al menos un dígito
+                    message=('The password must contain at least one digit.')
+                ),
+                RegexValidator(
+                    regex=r'^(?=.[@$!%?&])',  # Al menos un carácter especial
+                    message=('The password must contain at least one special character (@$!%*?&).')
+                ),
+                RegexValidator(
+                    regex=r'^.{8,}$',  # Al menos 8 caracteres de longitud
+                    message=('The password must be at least 8 characters long.')
+                )
+            ])
+        validated_data['password'] = make_password(validated_data.get ( 'password'))
         return super(ProfileCrationSerializer, self).create(validated_data)
+    
+        
