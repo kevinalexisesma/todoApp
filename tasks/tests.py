1c6e55c0352bd5e22cc392b43bacec5a99c1c28b
MarlonDiaz2022 <100392114+MarlonDiaz2022@users.noreply.github.com>
2024-06-18 21:27:10 -0500

    fix: removing bd and fixing conflicts

--- tasks/tests.py
+++ tasks/tests.py
@@ -2,11 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
-<<<<<<< Updated upstream
-from rest_framework.authtoken.models import Token
-=======
 from rest_framework_simplejwt.tokens import RefreshToken
->>>>>>> Stashed changes
 from .models import Task
 
 class TaskTest(APITestCase):  
@@ -15,15 +11,11 @@ class TaskTest(APITestCase):
     def setUpClass(cls):
         user_class = get_user_model()
         cls.user = user_class.objects.create(username="john", password="1234",email="foo@bar.com",bio="nothing")
-<<<<<<< Updated upstream
-        cls.token.objects.create(user=cls.user)
-=======
         
         # Generación del token JWT
         refresh = RefreshToken.for_user(cls.user)
         cls.token = str(refresh.access_token)
         
->>>>>>> Stashed changes
         cls.task = Task.objects.create(
             name="My Task", description="My task description", user=cls.user
         )
@@ -48,9 +40,4 @@ class TaskTest(APITestCase):
         url=reverse("task-detail", kwargs={"pk":self.task.id})
         response= self.client.get(url,format="json")
         self.assertequal(response.status_code, status.HTTP_200_OK)
-<<<<<<< Updated upstream
-        self.asserequal(len(response.data.get("name")),self.task.name)
-=======
         self.asserequal(len(response.data.get("name")),self.task.name) 
-    
->>>>>>> Stashed changes
