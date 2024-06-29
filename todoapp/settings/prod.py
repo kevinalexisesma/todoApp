0ed914caebbc4416a8b217a55a00ec3a6b576c9e
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2024-06-28 19:36:53 -0500

    Fix(host)

--- todoapp/settings/prod.py
+++ todoapp/settings/prod.py
@@ -10,7 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS")
 
 CORS_ALLOW_ALL_ORIGINS = True
 
